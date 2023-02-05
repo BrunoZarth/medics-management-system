767460d80199d5fad7fe19b5fd7eb9e25e921074
BrunoZarth <brunozarth@gmail.com>
2023-02-05 01:35:04 -0300

    fix: wrong sql values

--- back-end/medics-management/src/main/resources/data.sql
+++ back-end/medics-management/src/main/resources/data.sql
@@ -1,9 +1,11 @@
 insert into medic (adress, cep, crm, landline, name, phone)
-values ('Exile street, 1972', '50000123', '1597539', '5133374717', 'Keith Richards', '51955668899');
+values ('Exile street, 1972', '94444000', '159753', '51333747177', 'Keith Richards', '51955668899');
 insert into medic_medical_specialty (medic_id, medical_specialty) values (1, 2);
 insert into medic_medical_specialty (medic_id, medical_specialty) values (1, 3);
 
 insert into medic (adress, cep, crm, landline, name, phone)
-values ('Sunset Strip, 77', '95555000', '2618640', '33256987', 'Lemmy Kilmister', '59648755532');
+values ('Sunset Strip, 77', '95555000', '261864', '51332569867', 'Lemmy Kilmister', '59648755532');
 insert into medic_medical_specialty (medic_id, medical_specialty) values (2, 4);
 insert into medic_medical_specialty (medic_id, medical_specialty) values (2, 5);
+
+
